7b29a3e74e706d55459bf0240c87d9a3f92a89d9
Ваше Имя <hogben_mad@mail.ru>
2024-09-08 11:00:35 +0300

    2024-09-08

--- bigNumner.cpp
+++ bigNumner.cpp
@@ -12,19 +12,58 @@ class BigNumber
         BigNumber(int arg)      { s_str << arg; init(s_str.str()); }  
         BigNumber(long arg)     { s_str << arg; init(s_str.str()); }  
 
+        BigNumber& _pointer()   { return *this; }
+
         void init(string arg);
         void view();
 
-        string sum(string val, BigNumber* bn);
-        string sub(string val, BigNumber* bn);
+        friend bool operator == (const BigNumber& a, const BigNumber& b) 
+        {
+           if (a.positive == b.positive && a.digit.size() == b.digit.size())
+           {
+                for (int i = 0; i < a.digit.size(); i++)
+                {
+                    if (a.digit[i] != b.digit[i])   return false;
+                }       
+                return true;
+           }
+           return false;
+        }
+
+        friend bool operator > (const BigNumber& a, const BigNumber& b) 
+        {   
+            if (a == b) return false;
+            if (a.positive && !b.positive)  return true;
+            if (!a.positive && b.positive)  return false;
+            if (a.digit.size() > b.digit.size())    return a.positive;
+            if (a.digit.size() < b.digit.size())    return !a.positive;
+            for (int i = a.digit.size(); i > 0; i--)
+            {
+                if (a.digit[i-1] > b.digit[i-1])    return a.positive;
+                if (a.digit[i-1] < b.digit[i-1])    return !a.positive;
+            }
+            return false;
+        }
+
+        friend bool operator < (const BigNumber& a, const BigNumber& b) 
+        {   
+            return (!(a > b));
+        }
+
+        friend ostream& operator << (ostream &ostrm, const BigNumber& a) { return ostrm << (a.positive ? "" : "-") << a.value; }
+
+        BigNumber& add(BigNumber* bn);
+        BigNumber& sub(BigNumber* bn);
+        
+        BigNumber& multi(BigNumber* bn);
 
         vector<short>   digit;
         string GetValue() { return value; };
         bool IsPositive() { return positive; };
+        bool positive = true;
     protected:
         stringstream s_str;   
         string value;
-        bool positive = true;
 };
 
 void BigNumber::init(string arg)
@@ -45,49 +84,43 @@ void BigNumber::init(string arg)
 void BigNumber::view()
 {
     if (!IsPositive())  cout << "-";
-    for(int i = digit.size(); i > 0; i--)
-    {
-        cout << digit[i-1];
-    }
+    cout << value;
     cout << endl;
 }
 
-string BigNumber::sum(string s_arg, BigNumber* arg)
+BigNumber& BigNumber::add(BigNumber* arg)
 {
     vector<short>    res;
     int max, min;
     short carry = 0;
-    BigNumber *t_num = new BigNumber(s_arg);
     stringstream sstr;   
 
-//    t_num->view();
-//    arg->view();
     sstr.str("");
 
-    if (t_num->IsPositive() && arg->IsPositive())
+    if (IsPositive() && arg->IsPositive())
     {
-        if  (t_num->digit.size() > arg->digit.size()) 
+        if  (digit.size() > arg->digit.size()) 
         {
             min = arg->digit.size();
-            max = t_num->digit.size();
+            max = digit.size();
         }
         else
         {
             max = arg->digit.size();
-            min = t_num->digit.size();
+            min = digit.size();
         }
         int idx;
         short t_short;
         for (idx = 0; idx < min; idx++)
         {
-            t_short = t_num->digit[idx] +  arg->digit[idx] + carry;
+            t_short = digit[idx] +  arg->digit[idx] + carry;
             carry = (t_short > 9) ? 1 : 0;
             res.push_back(t_short%10);
         }    
         for (idx = min; idx < max; idx++)
         {
-            if (max == t_num->digit.size())
-                t_short = t_num->digit[idx] + carry;
+            if (max == digit.size())
+                t_short = digit[idx] + carry;
             else
                 t_short = arg->digit[idx] + carry;
             carry = (t_short > 9) ? 1 : 0;
@@ -101,39 +134,126 @@ string BigNumber::sum(string s_arg, BigNumber* arg)
             sstr << res[i-1];
         }
 
-        return sstr.str();
+        init(sstr.str());
     }
     else
     {
-        if (t_num->IsPositive() && !arg->IsPositive())
+        if (IsPositive() && !arg->IsPositive())
         {
-            return(sub(t_num->GetValue(), new BigNumber(arg->GetValue())));
+            sub(new BigNumber(arg->GetValue()));
         }
         else
         {
-            if (!t_num->IsPositive() && arg->IsPositive())
+            if (!IsPositive() && arg->IsPositive())
             {   
-                return(sub(arg->GetValue(), new BigNumber(t_num->GetValue())));
+                arg->sub(new BigNumber(GetValue()));
             }
             else
             {
-                sstr << "-";
-                sstr << sum(t_num->GetValue(), new BigNumber(arg->GetValue()));
-                return sstr.str();
+                BigNumber* n1 = new BigNumber(GetValue());
+                n1->add(new BigNumber(arg->GetValue()));
+                sstr << "-" << n1->GetValue();
+                init (sstr.str());
+            }
+        }
+    }
+    return *this;
+}
+
+BigNumber& BigNumber::sub(BigNumber *b)
+{   
+    if (*this == *b)
+    {
+        init("0");
+    }
+    else
+    {
+        if (!b->IsPositive()) add(new BigNumber(b->GetValue()));
+        else
+        {
+            if (!positive)
+            {
+                positive = true;
+                add(new BigNumber(b->GetValue()));
+                positive = false;   
+            }
+            else
+            {       
+                vector<short>    res;
+                BigNumber *n1, *n2;
+                int min, max;
+                stringstream sstr;   
+
+                bool need_negative = false;
+
+                if (*this > *b)
+                {
+                    max = digit.size();
+                    min = b->digit.size();
+                    n1 = new BigNumber(GetValue());
+                    n2 = new BigNumber(b->GetValue());
+                }
+                else
+                {
+                    need_negative = true;
+                    min = digit.size();
+                    max = b->digit.size();
+                    n1 = new BigNumber(b->GetValue());
+                    n2 = new BigNumber(GetValue());
+                }
+                int j;
+                int i;
+                for (i = 0; i < min; i++)
+                {
+                    if (n1->digit[i] < n2->digit[i])
+                    {
+                        res.push_back((n1->digit[i] +10) - n2->digit[i]);  
+                        j = i + 1;
+                        while (j < max)
+                        {
+                            if (n1->digit[j] > 0)
+                            {
+                                n1->digit[j]--;    
+                                break;
+                            }
+                            else
+                            {
+                                n1->digit[j] = 9;
+                                j++;    
+                            }
+                        }  
+                    }
+                    else
+                    {
+                        res.push_back(n1->digit[i] - n2->digit[i]);
+                    }
+                }
+                for (; i < max; i++)
+                    res.push_back(n1->digit[i]);
+
+                bool print_sym = false;
+
+                for(int i = res.size(); i > 0; i--)
+                {
+                    if (res[i-1] > 0)  print_sym = true;
+                    if (print_sym)  sstr << res[i-1];
+                }
+
+                init(sstr.str());
+                if (need_negative)  positive = false;
+
             }
         }
     }
 
-}
-
-string BigNumber::sub(string a, BigNumber *b)
-{
-    if (!b->IsPositive()) return sum(a, b);
-    return "sub";
+    return *this;
 }
 
 int main()
 {
-    BigNumber *n1 = new BigNumber(-11);
-    cout << BigNumber(32233232).sum("-11", new BigNumber(-11)) << endl;
+                                     
+    BigNumber *n1 = new BigNumber("10000000000000000000000000000000000000000000");
+
+    cout << n1->sub(new BigNumber(11)) << endl;
+
 }
